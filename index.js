44e3af31bcfbf3fa808fa8333b5b4aff59407fbd
Manjunath <manjug@microsoft.com>
2022-08-05 14:39:53 +0530

    index.js file

---
+++ index.js
@@ -0,0 +1,7 @@
+const http = require('http')
+const server = http.createServer(function (req, res) {
+res.end(`Here's your coffee. Pura Vida\n`)
+})
+server.listen(8080, function () {
+console.log(`listening on 8080`)
+})
